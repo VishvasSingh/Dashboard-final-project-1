4a98a609853923cbe2556f13f9dc52a148272eaa
vishvassingh <vishvassingh.10816@gmail.com>
2024-02-25 18:57:27 +0530

    data input changes and spinner transparent background

--- src/app/pages/feature/project/feature/data-input/data-input.component.ts
+++ src/app/pages/feature/project/feature/data-input/data-input.component.ts
@@ -1,14 +1,32 @@
 import { Component } from "@angular/core";
-import { NbCardModule } from "@nebular/theme";
+import { NbButtonModule, NbCardModule, NbIconModule, NbInputModule } from "@nebular/theme";
+import { HttpService } from "src/app/http/services/http.service";
 
 @Component({
     selector: 'app-data-input',
     templateUrl: 'data-input.component.html',
     styleUrls: ['data-input.component.scss'],
     standalone: true,
-    imports: [NbCardModule, ]
+    imports: [NbCardModule, NbInputModule, NbButtonModule, NbIconModule]
 })
 
 export class DataInputComponent {
+    selectedFile: File | null = null;
+    constructor(private http: HttpService){}
 
+    onFileSelected(event: any) {
+        this.selectedFile = event.target.files[0] as File;
+    }
+
+    uploadFile(){
+        if (!this.selectedFile) {
+            console.error('No Files selected');
+            return ;
+        }
+
+        const formData = new FormData();
+        formData.append('file', this.selectedFile);
+
+        console.log(formData)
+    }
 }
